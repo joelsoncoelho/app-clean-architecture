693e6129e85e294833b63ca08075b0b40e44db1a
Joelson Coelho <joelson.save@gmail.com>
2024-11-17 13:16:06 -0300

    first commit

---
+++ src/main/java/com/projeto/clean/architecture/app_clean_architecture/AppCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.clean.architecture.app_clean_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCleanArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCleanArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/clean/architecture/app_clean_architecture/AppCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.clean.architecture.app_clean_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
